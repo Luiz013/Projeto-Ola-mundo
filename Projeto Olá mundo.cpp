54ed97fcee43eabc2ef8d673cd6e0dc861ca800e
Luiz <Luhpaulo2016@hotmail.com>
2020-05-30 19:16:47 -0300

    Ola mundo

---
+++ Projeto Olá mundo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+float soma(float a,float b) {
+	return a+b;
+}
+
+int main(){
+	std::cout <<"Ola mundo" <<std::endl;
+   std::cout<<soma(10,5.6)<<std::endl;
+	 return 0;
+}
